6b9f154985aaad35e4cb38e4f97c305721feec7d
林湘杭 <1320081172@qq.com>
2023-04-23 18:16:06 +0800

    4.23 Test Next

--- 2023.4.10/Main/src/Test2.java
+++ 2023.4.10/Main/src/Test2.java
@@ -1,3 +1,4 @@
+import com.bit.www.Test;
 import com.sun.deploy.perf.PerfRollup;
 
 /**
@@ -7,11 +8,34 @@ import com.sun.deploy.perf.PerfRollup;
  * @Date 2023/4/12 15:45
  **/
 class People{
+    /**
+        静态代码块 > 实例代码块 > 构造方法
+     <注意>：字节码层面<clinit>是Java中的一个特殊方法，也称为类初始化方法。它是在类加载时由Java虚拟机调用的，
+     用于执行静态初始化代码块和静态变量初始化。如果类没有静态初始化代码块或静态变量，则不会生成<clinit>方法。
+     **/
     public static String emotion = "Nice";
     public int count;
+    public static int score;
+
+    class InClass{
+        //InClass类相对于People类来说是内部类，而People类对于InClass来说是外部类
+        public int a;
+    }
+
+    static {
+        //静态代码块的优先级高于实例代码块【静态代码块无论定义了多少个对象，最终只执行一个】
+        score = 100;
+        System.out.println("shit!");
+    }
+
+    {
+        //实例代码块优先于构造方法
+        System.out.println("fuck you");
+    }
 
     public People(int count) {
         this.count = count;
+        System.out.println("good!");
     }
 
     @Override
@@ -29,9 +53,7 @@ class People{
 public class Test2 {
     public static void main(String[] args) {
         People people = new People(5);
-        People people1 =new People(6);
-        System.out.println(People.emotion);
-        System.out.println(people.emotion);
-
+        Test test = new Test();
+        test.scan2();
     }
 }
---
+++ 2023.4.10/Main/src/com/bit/www/Test.java
@@ -0,0 +1,87 @@
+package com.bit.www;
+
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static java.lang.System.exit;
+
+/**
+ * @ClassName Test
+ * @Description TODO
+ * @Author 86189
+ * @Date 2023/4/23 16:35
+ **/
+public class Test {
+    public void scan1(){
+        Scanner scanner =new Scanner(System.in);
+        int a = 0;
+        a = scanner.nextInt();
+        System.out.println(a);
+    }
+    public void scan2(){
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String str = null;
+        try {
+            while((str = reader.readLine())!=null && !str.isEmpty()){
+                System.out.println(str);
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    public void scan3(){
+        JFrame frame = new JFrame();
+        int a = Integer.valueOf(JOptionPane.showInputDialog("Enter:",0));
+        String str =  JOptionPane.showInputDialog("请输入运算符:","+");
+        int b = Integer.valueOf(JOptionPane.showInputDialog("Enter:",0));
+        int sum = 0;
+        switch(str){
+            case "+":{
+                sum = a+b;
+                break;
+            }
+            case "-":{
+                sum = a-b;
+                break;
+            }
+            case "*":{
+                sum = a*b;
+                break;
+            }
+            case "/":{
+                sum = a/b;
+                break;
+            }
+            default:break;
+        }
+        JOptionPane.showMessageDialog(frame,sum);
+        exit(0);
+    }
+
+    public static void binarySearch(int []arr,int des){
+        int left = 0,right=arr.length-1;
+        int mid = 0;
+        while(left<=right){
+            mid = ((right-left)>>1) + left;
+            if(arr[mid]>des){
+                right = mid-1;
+            }else if(arr[mid]<des){
+                left = mid+1;
+            }else{
+                System.out.println("Success!");
+                return;
+            }
+        }
+        System.out.println("Fail!");
+    }
+    public static void main(String[] args) {
+        int []arr = {1,2,3,4,5,6};
+        binarySearch(arr,5);
+    }
+}
